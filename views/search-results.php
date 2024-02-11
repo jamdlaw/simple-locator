6264df4b17880ed1266f101e17233ccf3a9173fb
James Lawrence <jamdlaw@yahoo.com>
2024-02-10 19:44:01 -0800

    change order of output

--- views/search-results.php
+++ views/search-results.php
@@ -19,9 +19,9 @@ if ( $has_results ) :
 	$output .= '<div id="gc-map-and-results" >'; 
 	$results_output = '<div class="wpsl-results-wrapper">';
 	foreach($this->search_data['results'] as $result) :
+		$results_output .= '<div>' . get_the_post_thumbnail($result['id']) . '</div>';
 		//$result['output'] is a html <p> tag
 		$results_output .= $result['output'];
-		$results_output .= '<div>' . get_the_post_thumbnail($result['id']) . '</div>';
 		$results_output .= '<div>' .  do_shortcode( '[site_review post_id="658" hide="assigned_links,author,avatar,content,date,images,response,title"]' ) . '</div>';  
 	endforeach;
 	$results_output .= '</div>';
